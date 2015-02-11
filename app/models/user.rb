c90db77bd306546dfac7b39c0b73e419468d8e49
Lydias303 <shaferlydia@yahoo.com>
2015-02-11 15:21:41 -0700

    fuck you hound

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ActiveRecord::Base
             format: {
               with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
             },
-            uniqueness: true
-            
+            uniqueness: true            
   has_many :orders
 
   def admin?
