ed4e7858c35e9f3e61bb69949b151e5695ec98ea
Lydias303 <shaferlydia@yahoo.com>
2015-02-11 22:56:28 -0700

    fixed tests

--- spec/feature/authenticated_user_spec.rb
+++ spec/feature/authenticated_user_spec.rb
@@ -168,25 +168,25 @@ describe "an authenticated user" do
     end
   end
 
-  it "can view past orders with links to each order" do
+  it "can view past trips with links to each trip" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).
                                                     and_return(valid_user)
     Order.create(user_id: valid_user.id)
     Order.create(user_id: valid_user.id)
     visit user_path(valid_user.id)
-    click_link_or_button "View past orders"
+    click_link_or_button "My Trips"
     expect(current_path).to eq(orders_path)
     within(".orders-list") do
       expect(page).to have_content("Order 00001")
     end
   end
 
-  it "can view particular orders (order show page)" do
+  it "can view particular trips (trip show page)" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).
     and_return(valid_user)
     Order.create(user_id: valid_user.id)
     visit user_path(valid_user.id)
-    click_link_or_button "View past orders"
+    click_link_or_button "My Trips"
     click_link_or_button "Order 00001"
     expect(page).to have_content("Order 00001")
   end
