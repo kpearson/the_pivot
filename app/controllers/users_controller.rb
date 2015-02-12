1efa0265f48954ec95cb19fb453d8b4e7e8c144a
Lydias303 <shaferlydia@yahoo.com>
2015-02-11 22:51:27 -0700

    paperclip user-profile user image and about me added to user table, awful attempt at a search bar, edit profile button with functionality, my trips button, half ass working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to :back
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :image, :about_me)
   end
 end
