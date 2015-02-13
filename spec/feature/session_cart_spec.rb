4a3637418a0be4d77c2eb086268b56425231800d
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-02-13 16:21:14 -0700

    Remove whitespace

--- spec/feature/session_cart_spec.rb
+++ spec/feature/session_cart_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "A session cart" do
   include Capybara::DSL
-  
+
   xit "instantiates a cart with a controller action" do
     visit root_path
     within("#cart-contents") do
