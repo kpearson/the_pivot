d89431d64ff7c6c662fc36bcc8be6681bf604583
Chris Luhring <cluhring@gmail.com>
2015-02-15 23:56:57 -0700

    a user can search listings by city, category, rate and guests

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,18 @@
 class ListingsController < ApplicationController
+  # def index
+  #   @listings = Listing.all
+  # end
+
   def index
-    @listings = Listing.all
+    @listings = Listing.where(nil)
+    filtering_params(params).each do |key, value|
+    @listings = @listings.public_send(key, value) if value.present?
+    end
+  end
+
+private
+
+  def filtering_params(params)
+    params.slice(:city, :max_guests, :category_id, :nightly_rate)
   end
 end
