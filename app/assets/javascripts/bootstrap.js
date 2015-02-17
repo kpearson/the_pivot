12f24523f728d8d86f8664b776d088532b638ce1
Kit Pearson <dev_kit@icloud.com>
2015-02-17 14:49:11 -0700

    Update bootstrap.js
    Damn you dubble quotes

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,11 +1,11 @@
 $( document ).ready(function() {
-  $('#signup-modal').on('show.bs.modal', function () {
+  $("#signup-modal").on("show.bs.modal", function () {
     $.get("/users/new").done(function (data) {
       $("#signup-modal .modal-body").html(data);
-      $('#signup-modal form').on('ajax:success', function(e, xhr) {
+      $("#signup-modal form").on("ajax:success", function(e, xhr) {
         window.location = "/?fromsignup=true"
       });
-      $('#signup-modal form').on('ajax:error', function(e, xhr) {
+      $("#signup-modal form").on("ajax:error", function(e, xhr) {
         $("#signup-modal .modal-body").html(xhr.responseText);
       });
     });
