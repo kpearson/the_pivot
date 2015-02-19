ced5c6a9f20d82ae202fa132c3f7c4db75be1c83
Chris Luhring <cluhring@gmail.com>
2015-02-19 12:14:51 -0700

    migrated orders to reservations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217001812) do
+ActiveRecord::Schema.define(version: 20150219191046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,13 +53,18 @@ ActiveRecord::Schema.define(version: 20150217001812) do
     t.integer  "zip"
     t.boolean  "shared_bathroom"
     t.integer  "user_id"
+    t.json     "images"
   end
 
-  create_table "orders", force: :cascade do |t|
+  create_table "reservations", force: :cascade do |t|
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.integer  "user_id"
-    t.string   "status",     default: "ordered"
+    t.integer  "guest_id"
+    t.string   "status",     default: "pending"
+    t.integer  "listing_id"
+    t.integer  "host_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
   end
 
   create_table "users", force: :cascade do |t|
