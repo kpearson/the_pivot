8ee05aaae7a1a86496721c3e93fea4f5bafecf42
Kit <dev_kit@icloud.com>
2015-02-19 23:11:51 -0700

    add edit listing

--- spec/feature/user/edits_a_listing_spec.rb
+++ spec/feature/user/edits_a_listing_spec.rb
@@ -9,7 +9,7 @@ describe 'user edits a listing', type: :feature do
     valid_user = create(:user)
     listing = create(:listing)
     allow_any_instance_of(ApplicationController).to receive(:current_user).
-                                                    and_return(valid_user)
+      and_return(valid_user)
     visit edit_user_listing_path(valid_user.slug, listing.id)
     expect(page).to have_content("Edit listing")
   end
@@ -19,7 +19,7 @@ describe 'user edits a listing', type: :feature do
     listing = create(:listing)
     create(:category, name: "Condo")
     allow_any_instance_of(ApplicationController).to receive(:current_user).
-                                                    and_return(valid_user)
+      and_return(valid_user)
     visit edit_user_listing_path(valid_user.slug, listing.id)
     fill_in "listing[title]", with: "Edited Listing"
     fill_in "listing[description]", with: "edited Listing description"
