758057a0ec3257b44034ebbb64e1f9a4198cb4e1
Kit <dev_kit@icloud.com>
2015-02-21 23:31:06 -0700

    add image uploader for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219053002) do
+ActiveRecord::Schema.define(version: 20150222060531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20150219053002) do
     t.string   "display_name"
     t.string   "slug"
     t.integer  "role",               default: 0
+    t.string   "image"
   end
 
 end
