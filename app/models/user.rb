fc73d695207b379c10842fc380f2024ddbfcb787
Kit <dev_kit@icloud.com>
2015-02-21 22:17:12 -0700

    tuning image uploader for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
                                       thumb: "100x100>" },
                                       default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  mount_uploader :image, UserUploader
 
   before_save :generate_slug
 
