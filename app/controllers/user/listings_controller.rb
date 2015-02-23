60d8c1bc64416f62fe38edc42d54bc6145297d4e
Lydias303 <shaferlydia@yahoo.com>
2015-02-22 23:27:58 -0700

    changed form to div

--- app/controllers/user/listings_controller.rb
+++ app/controllers/user/listings_controller.rb
@@ -22,7 +22,7 @@ class User::ListingsController < ApplicationController
       end
     end
     if @listing.save
-      current_user.update_attributes(role: 1)
+      current_user.update_attributes(:role => "host")
       redirect_to user_listing_path(current_user.slug, @listing.id)
     else
       render :new
