5fa0ac946513c3c81581a0945ca3c65b5cf4f96a
Lydias303 <shaferlydia@yahoo.com>
2015-02-22 17:12:35 -0700

    fixed silly delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,18 +11,9 @@ class User < ActiveRecord::Base
   has_many :listings
   has_many :listing_images, :through => :listings
   has_many :orders
-<<<<<<< HEAD
-  has_attached_file :image, styles: { medium: "300x300>",
-                                      thumb: "100x100>" },
-                                      default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   validates :password, presence: true, on: :create
   validates :password_confirmation, presence: true, on: :create
-=======
-
   mount_uploader :image, UserUploader
->>>>>>> 001e874480cccf1b3d8889c9a8df87340b60f2a0
-
   before_save :generate_slug
 
   enum role: [:default, :host]
