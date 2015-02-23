468194ed9da91b5422789ac409914a4e3ad1823e
Lydias303 <shaferlydia@yahoo.com>
2015-02-23 00:14:10 -0700

    added before filter for authorization of host dashboard

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,6 +1,13 @@
 class User::DashboardController < ApplicationController
+  before_action :require_host
 
-  def index 
+  def index
     @listings = Listing.all
+  end
+
+  private
+
+  def require_host
+    redirect_to root_path unless current_user && current_user.host? && current_user.id == params[:slug].to_i
   end
 end
