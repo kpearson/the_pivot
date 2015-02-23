26e55530efd6c2c1dabda2be84d60bbac6208a40
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-02-22 19:39:15 -0700

    comment out old order controller methods

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,29 +1,29 @@
 class ReservationsController < ApplicationController
   def new
-    @reservation = Reservation.new
-    @cart.data.each do |item_id, quantity|
-      item = Item.unscoped.find(item_id.to_i)
-      item.add_quantity(quantity)
-      @reservation.items << item
-    end
+    #@reservation = Reservation.new
+    #@cart.data.each do |item_id, quantity|
+      #item = Item.unscoped.find(item_id.to_i)
+      #item.add_quantity(quantity)
+      #@reservation.items << item
+    #end
   end
 
   def create
-    if current_user
-      @reservation = Reservation.new(user_id: current_user.id)
-      reservation_listings = @reservation.create_reservation_listings(@cart.data)
-      @reservation.reservation_listings << reservation_listings
-      if @reservation.save
-        flash[:notice] = "Your delicious food is on the way"
-        redirect_to reservation_path(@reservation.id)
-      else
-        flash[:error] = "System error, unable to place reservation"
-        redirect_to new_reservation_path
-      end
-    else
-      flash[:notice] = "Please login or signup to continue with checkout"
-      redirect_to :back
-    end
+    #if current_user
+      #@reservation = Reservation.new(user_id: current_user.id)
+      #reservation_listings = @reservation.create_reservation_listings(@cart.data)
+      #@reservation.reservation_listings << reservation_listings
+      #if @reservation.save
+        #flash[:notice] = "Your delicious food is on the way"
+        #redirect_to reservation_path(@reservation.id)
+      #else
+        #flash[:error] = "System error, unable to place reservation"
+        #redirect_to new_reservation_path
+      #end
+    #else
+      #flash[:notice] = "Please login or signup to continue with checkout"
+      #redirect_to :back
+    #end
   end
 
   def index
